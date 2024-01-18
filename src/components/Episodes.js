5de3295a47fa9fecf3999a449a64bc0a265288e6
Clarisse Mille <cmille749@gmail.com>
2024-01-18 18:42:15 +0100

    episodes

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import Episode from './Episode';
+import '../styles/Episodes.css'
+import img from '../assets/img/bouteille.png';
+import img1 from '../assets/img/bouteille1.png';
+
 
 function Episodes() {
+    const episode1 = () => {
+        console.log("Episode 1");
+    }
+    const episode2 = () => {
+        console.log("Episode 2");
+    }
+    const episode3 = () => {
+        console.log("Episode 3");
+    }
+    const episode4 = () => {
+        console.log("Episode 4");
+    }
+    const episode5 = () => {
+        console.log("Episode 5");
+    }
+    const episode6 = () => {
+        console.log("Episode 6");
+    }
+
     return (
-        <div>
+        <div className='grille'>
+            <div className="episode"> <img src={img} onClick={episode1} alt="" /></div>
+            <div className="episode"> <img src={img1} onClick={episode2} alt="" /></div>
+            <div className="episode"> <img src={img} onClick={episode3} alt="" /></div>
+            <div className="episode"> <img src={img1} onClick={episode4} alt="" /></div>
+            <div className="episode"> <img src={img} onClick={episode5} alt="" /></div>
+            <div className="episode"> <img src={img1} onClick={episode6} alt="" /></div>
+          
+
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Episodes from '../components/Episodes';
+import '../styles/Home.css'
 
 function Home() {
     return (
         <div>
+            <h1>Liste des épisodes</h1>
             <Episodes/>
         </div>
     );
